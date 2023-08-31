4228365adebc13a4730576d1592be3bf2f124729
Bhargav Joshi <bhargav.joshi@growexx.com>
2023-08-31 19:48:37 +0530

    Removed comments

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -8,37 +8,37 @@ describe('App renders successfully', () => {
   })
 
   test('on successfully render on production', async () => {
-    // jest.mock('react-native/Libraries/Utilities/Platform', () => ({
-    //   OS: 'android',
-    //   select: () => null
-    // }))
-    // jest.mock('react-native-config', () => {
-    //   return {
-    //     ENV: 'Production',
-    //     API_URL: 'https://newsapi.org',
-    //     CODE_PUSH_KEY_IOS: '2Dx0tSAxO1QN1u',
-    //     CODE_PUSH_KEY_ANDROID: '8vQY82WtSsdZk8VIoM',
-    //     GOOGLE_CLIENT_ID_ANDROID: '210784425858',
-    //     GOOGLE_CLIENT_ID_IOS: '210784425858'
-    //   }
-    // })
-    // const { getByTestId } = render(<App />)
+    jest.mock('react-native/Libraries/Utilities/Platform', () => ({
+      OS: 'android',
+      select: () => null
+    }))
+    jest.mock('react-native-config', () => {
+      return {
+        ENV: 'Production',
+        API_URL: 'https://newsapi.org',
+        CODE_PUSH_KEY_IOS: '2Dx0tSAxO1QN1u',
+        CODE_PUSH_KEY_ANDROID: '8vQY82WtSsdZk8VIoM',
+        GOOGLE_CLIENT_ID_ANDROID: '210784425858',
+        GOOGLE_CLIENT_ID_IOS: '210784425858'
+      }
+    })
+    const { getByTestId } = render(<App />)
   })
-  // test('on successfully render on development', async () => {
-  //   jest.mock('react-native/Libraries/Utilities/Platform', () => ({
-  //     OS: 'ios',
-  //     select: () => null
-  //   }))
-  //   jest.mock('react-native-config', () => {
-  //     return {
-  //       ENV: 'Development',
-  //       API_URL: 'https://newsapi.org',
-  //       CODE_PUSH_KEY_IOS: '2Dx0tSAxO1QN1u',
-  //       CODE_PUSH_KEY_ANDROID: '8vQY82WtSsdZk8VIoM',
-  //       GOOGLE_CLIENT_ID_ANDROID: '210784425858',
-  //       GOOGLE_CLIENT_ID_IOS: '210784425858'
-  //     }
-  //   })
-  //   const { getByTestId } = render(<App />)
-  // })
+  test('on successfully render on development', async () => {
+    jest.mock('react-native/Libraries/Utilities/Platform', () => ({
+      OS: 'ios',
+      select: () => null
+    }))
+    jest.mock('react-native-config', () => {
+      return {
+        ENV: 'Development',
+        API_URL: 'https://newsapi.org',
+        CODE_PUSH_KEY_IOS: '2Dx0tSAxO1QN1u',
+        CODE_PUSH_KEY_ANDROID: '8vQY82WtSsdZk8VIoM',
+        GOOGLE_CLIENT_ID_ANDROID: '210784425858',
+        GOOGLE_CLIENT_ID_IOS: '210784425858'
+      }
+    })
+    const { getByTestId } = render(<App />)
+  })
 })
